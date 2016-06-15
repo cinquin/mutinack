106f31e5a31e6c43d8d55d1484029b94ed6aec22
Olivier Cinquin <olivier.cinquin@uci.edu>
2016-06-15 09:13:13 -0700

    Add missing type.

--- src/uk/org/cinquin/mutinack/misc_util/collections/TroveSetCollector.java
+++ src/uk/org/cinquin/mutinack/misc_util/collections/TroveSetCollector.java
@@ -1,16 +1,16 @@
 /**
  * Mutinack mutation detection program.
  * Copyright (C) 2014-2016 Olivier Cinquin
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, version 3.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,9 +25,9 @@ import gnu.trove.set.hash.THashSet;
 public class TroveSetCollector {
 	public static<T> Collector<T, Set<T>, Set<T>> uniqueValueCollector() {
 	    return Collector.of(
-	            THashSet::new,
+	            THashSet<T>::new,
 	            (set, e) -> set.add(e),
-	            (set1, set2) -> { 
+	            (set1, set2) -> {
 	            	set1.addAll(set2);
 	            	return set1;
 	            	},
