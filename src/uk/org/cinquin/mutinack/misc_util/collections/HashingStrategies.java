1f8acdc0d10ab7c401d8eb46be6966003d867da2
Olivier Cinquin <olivier.cinquin@uci.edu>
2016-06-15 19:56:42 -0700

    Add missing file.

---
+++ src/uk/org/cinquin/mutinack/misc_util/collections/HashingStrategies.java
@@ -0,0 +1,22 @@
+package uk.org.cinquin.mutinack.misc_util.collections;
+
+import gnu.trove.strategy.HashingStrategy;
+import uk.org.cinquin.mutinack.DuplexRead;
+
+public class HashingStrategies {
+
+	public static final HashingStrategy<DuplexRead> identityHashingStrategy = 
+		new HashingStrategy<DuplexRead>() {
+		private static final long serialVersionUID = -5196382307004964827L;
+
+		@Override
+		public int computeHashCode(DuplexRead arg0) {
+			return System.identityHashCode(arg0);
+		}
+
+		@Override
+		public boolean equals(DuplexRead arg0, DuplexRead arg1) {
+			return arg0 == arg1;
+		}
+	};
+}
