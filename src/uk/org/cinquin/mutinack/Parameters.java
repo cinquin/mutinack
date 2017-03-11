b6da4652f1cba08b15b6a92cf2a6a10572906f74
Olivier Cinquin <olivier.cinquin@uci.edu>
2017-03-11 13:58:50 -0800

    Remove unnecessary parameter.

--- src/uk/org/cinquin/mutinack/Parameters.java
+++ src/uk/org/cinquin/mutinack/Parameters.java
@@ -556,7 +556,7 @@ public final class Parameters implements Serializable, Cloneable {
 	@Parameter(names = "-saveFilteredReadsTo", description = "Not implemented; write raw reads that were kept for analysis to specified files", required = false, hidden = hideInProgressParameters)
 	public List<@NonNull String> saveFilteredReadsTo = new ArrayList<>();
 
-	@Parameter(names = "-collapseFilteredReads", description = "Only write one (randomly-chosen) read pair per duplex strand", required = false, hidden = false)
+	@Parameter(names = "-collapseFilteredReads", description = "Only write one (randomly-chosen) read pair per duplex strand", required = false)
 	public boolean collapseFilteredReads = false;
 
 	@Parameter(names = "-writeBothStrands", description = "Used in conjunction with -collapseFilteredReads; write read pairs from both the top and the bottom strand, when available", required = false, hidden = false)
