91b6de5ececc23659333e10e94a871e2263af527
Olivier Cinquin <olivier.cinquin@uci.edu>
2017-07-11 22:28:18 -0700

    Formatting.

--- src/uk/org/cinquin/mutinack/SubAnalyzer.java
+++ src/uk/org/cinquin/mutinack/SubAnalyzer.java
@@ -822,8 +822,7 @@ public final class SubAnalyzer {
 		SettableInteger index = new SettableInteger(0);
 		duplexReads.forEach(duplexRead -> {
 			Assert.isFalse(duplexRead.invalid);
-			Assert.isTrue(duplexRead.averageNClipped >= 0,
-				() -> duplexRead.toString());
+			Assert.isTrue(duplexRead.averageNClipped >= 0, () -> duplexRead.toString());
 			Assert.isTrue(param.variableBarcodeLength > 0 ||
 				Double.isNaN(duplexRead.probAtLeastOneCollision) ||
 				duplexRead.probAtLeastOneCollision >= 0);
