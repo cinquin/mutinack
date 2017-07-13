fcee3100b6d1e21ab7206d20809171c91afad2df
Olivier Cinquin <olivier.cinquin@uci.edu>
2017-07-13 14:21:54 -0700

    Fix probable bug in jzlib pointed out by ErrorProne.

--- src/contrib/com/jcraft/jzlib/DeflaterOutputStream.java
+++ src/contrib/com/jcraft/jzlib/DeflaterOutputStream.java
@@ -93,7 +93,7 @@ public class DeflaterOutputStream extends FilterOutputStream {
     if (deflater.finished()) {
       throw new IOException("finished");
     }
-    else if (off<0 | len<0 | off+len>b.length) {
+    else if (off<0 || len<0 || off+len>b.length) {
       throw new IndexOutOfBoundsException();
     }
     else if (len == 0) {
