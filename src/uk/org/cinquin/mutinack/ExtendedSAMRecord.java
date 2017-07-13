815658f60c431c434a01132dd234d1899208f30c
Olivier Cinquin <olivier.cinquin@uci.edu>
2017-07-13 12:12:23 -0700

    Fix read identification problem when there are supplementary alignments to the same position on different reference sequences (potential problem with the HLA decoy contigs in the human genome). Adjust output of tests whose results are not stable when iteration order varies.

--- src/uk/org/cinquin/mutinack/ExtendedSAMRecord.java
+++ src/uk/org/cinquin/mutinack/ExtendedSAMRecord.java
@@ -100,9 +100,10 @@ public final class ExtendedSAMRecord implements HasInterval<Integer> {
 	private final @NonNull Mutinack analyzer;
 
 	public static @NonNull String getReadFullName(SAMRecord rec, boolean getMate) {
-		return (rec.getReadName() + "--" + ((getMate ^ rec.getFirstOfPairFlag())? "1" : "2") + "--" +
-			(getMate ? rec.getMateAlignmentStart() : rec.getAlignmentStart())) +
-			(!getMate && rec.getSupplementaryAlignmentFlag() ? "--suppl" : "")/*.intern()*/;
+		return (   rec.getReadName() + "--" + ((getMate ^ rec.getFirstOfPairFlag())? "1" : "2") + "--" +
+			(getMate ? rec.getMateReferenceName() : rec.getReferenceName()) +
+			(getMate ? rec.getMateAlignmentStart() : rec.getAlignmentStart()) +
+			(!getMate && rec.getSupplementaryAlignmentFlag() ? "--suppl" : "")   ) /*.intern()*/;
 	}
 
 	public @NonNull String getFullName() {
