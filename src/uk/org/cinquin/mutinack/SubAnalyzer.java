af21ae9ec4ad5b55a35871c74e91aa1302e52c48
Olivier Cinquin <olivier.cinquin@uci.edu>
2017-08-12 07:27:34 -0700

    Add detail to failed assertion.

--- src/uk/org/cinquin/mutinack/SubAnalyzer.java
+++ src/uk/org/cinquin/mutinack/SubAnalyzer.java
@@ -864,7 +864,9 @@ public final class SubAnalyzer {
 		SettableInteger index = new SettableInteger(0);
 		duplexes.forEach(duplexRead -> {
 			Assert.isFalse(duplexRead.invalid);
-			Assert.isTrue(duplexRead.averageNClipped >= 0, duplexRead::toString);
+			if (duplexRead.averageNClipped < 0) {
+				System.err.println("averageNClipped: " + duplexRead.averageNClipped + " for " + duplexRead);
+			}
 			Assert.isTrue(param.variableBarcodeLength > 0 ||
 				Double.isNaN(duplexRead.probAtLeastOneCollision) ||
 				duplexRead.probAtLeastOneCollision >= 0);
