11e07341a9ba614b9aa90dca086a60fa83a210e7
Olivier Cinquin <olivier.cinquin@uci.edu>
2017-08-24 10:10:38 -0700

    Formatting.

--- src/uk/org/cinquin/mutinack/misc_util/UtilTest.java
+++ src/uk/org/cinquin/mutinack/misc_util/UtilTest.java
@@ -27,67 +27,67 @@ public class UtilTest {
 
 	@Test
 	public void testBasesEqualByteByteBoolean() {
-	    assertFalse(Util.basesEqual((byte) 'a', (byte) 'b', true));
-	    assertTrue(Util.basesEqual((byte) 'a', (byte) 'N', true));
-	    assertTrue(Util.basesEqual((byte) 'a', (byte) 'a', true));
-	    assertTrue(Util.basesEqual((byte) 'N', (byte) 'N', true));
+		assertFalse(Util.basesEqual((byte) 'a', (byte) 'b', true));
+		assertTrue(Util.basesEqual((byte) 'a', (byte) 'N', true));
+		assertTrue(Util.basesEqual((byte) 'a', (byte) 'a', true));
+		assertTrue(Util.basesEqual((byte) 'N', (byte) 'N', true));
 	}
 
 	@Test
 	public void testBasesEqualByteArrayByteArrayBooleanInt() {
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 0));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 1));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 2));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, Integer.MAX_VALUE));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 0));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 2));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, Integer.MAX_VALUE));
 
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 0));
-	    assertFalse(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, true, 0));
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, true, 0));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 0));
+		assertFalse(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, true, 0));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, true, 0));
 
-	    assertTrue(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 1));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, true, 1));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertTrue(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
 
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, true, 1));
-	    assertFalse(Util.basesEqual(new byte[] {'a', 'd', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertFalse(Util.basesEqual(new byte[] {'a', 'd', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
 
-	    //Disallow Ns
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 0));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 1));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 2));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, Integer.MAX_VALUE));
+		//Disallow Ns
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 0));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 1));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 2));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, Integer.MAX_VALUE));
 
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 0));
-	    assertFalse(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, false, 0));
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, false, 0));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 0));
+		assertFalse(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, false, 0));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, false, 0));
 
-	    assertTrue(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 1));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, false, 1));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, false, 1));
+		assertTrue(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'b', 'c'}, false, 1));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, false, 1));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, false, 1));
 
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, false, 1));
-	    assertFalse(Util.basesEqual(new byte[] {'a', 'd', 'd'}, new byte[] {'a', 'b', 'c'}, false, 1));
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, false, 1));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'a', 'b', 'c'}, false, 1));
+		assertFalse(Util.basesEqual(new byte[] {'a', 'd', 'd'}, new byte[] {'a', 'b', 'c'}, false, 1));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'd'}, new byte[] {'a', 'b', 'c'}, false, 1));
 
-	    //Include Ns
+		//Include Ns
 
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'N', 'c'}, true, 0));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'N', 'b', 'N'}, true, 1));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'N', 'N', 'N'}, true, 2));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'N', 'b', 'c'}, true, Integer.MAX_VALUE));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'a', 'N', 'c'}, true, 0));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'N', 'b', 'N'}, true, 1));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'N', 'N', 'N'}, true, 2));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'b', 'c'}, new byte[] {'N', 'b', 'c'}, true, Integer.MAX_VALUE));
 
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'N', 'c'}, true, 0));
-	    assertFalse(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'N', 'b', 'c'}, true, 0));
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'N', 'b', 'c'}, true, 0));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'a', 'N', 'c'}, true, 0));
+		assertFalse(Util.basesEqual(new byte[] {'a', 'b', 'd'}, new byte[] {'N', 'b', 'c'}, true, 0));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'c'}, new byte[] {'N', 'b', 'c'}, true, 0));
 
-	    assertTrue(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'N', 'b', 'c'}, true, 1));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'd', 'c'}, new byte[] {'N', 'b', 'N'}, true, 1));
-	    assertTrue(Util.basesEqual(new byte[] {'a', 'N', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertTrue(Util.basesEqual(new byte[] {'d', 'b', 'c'}, new byte[] {'N', 'b', 'c'}, true, 1));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'd', 'c'}, new byte[] {'N', 'b', 'N'}, true, 1));
+		assertTrue(Util.basesEqual(new byte[] {'a', 'N', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
 
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'N'}, new byte[] {'a', 'b', 'c'}, true, 1));
-	    assertFalse(Util.basesEqual(new byte[] {'N', 'd', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
-	    assertFalse(Util.basesEqual(new byte[] {'d', 'N', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'd', 'N'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertFalse(Util.basesEqual(new byte[] {'N', 'd', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
+		assertFalse(Util.basesEqual(new byte[] {'d', 'N', 'd'}, new byte[] {'a', 'b', 'c'}, true, 1));
 
 	}
 
