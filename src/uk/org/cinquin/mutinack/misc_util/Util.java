70f478e762751f39959c9b7f9c6301b0d17d528e
Olivier Cinquin <olivier.cinquin@uci.edu>
2017-09-27 16:29:43 -0700

    Add information to message noting availability of update.

--- src/uk/org/cinquin/mutinack/misc_util/Util.java
+++ src/uk/org/cinquin/mutinack/misc_util/Util.java
@@ -527,7 +527,8 @@ public class Util {
 			String s = scanner.useDelimiter("\n").next();
 			double version = Double.parseDouble(s);
 			if (version > VersionNumber.version) {
-				System.err.println("New Mutinack version " + version + " is available at http://cinquin.org.uk/static/mutinack.jar");
+				System.err.println("New Mutinack version " + version + " is available at http://cinquin.org.uk/static/mutinack.jar ; " +
+					"current version is " + VersionNumber.version);
 			} else {
 				System.err.println("Mutinack is up to date");
 			}
