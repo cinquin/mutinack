bbbe68c4e92bf4dea206be1929042c22ee518c9b
Olivier Cinquin <olivier.cinquin@uci.edu>
2018-05-24 16:02:28 -0700

    Add missing doIntern.

--- src/uk/org/cinquin/mutinack/features/BedReader.java
+++ src/uk/org/cinquin/mutinack/features/BedReader.java
@@ -300,7 +300,7 @@ public class BedReader implements GenomeFeatureTester, Serializable {
 					}
 
 					final String annotations = annotationsColumn == -1 || annotationsColumn > components.length - 1 ? null
-						: components[annotationsColumn].intern();
+						: doIntern(internStrings, components[annotationsColumn]);
 
 					Integer contigIndex = reverseIndex.get(components[contigNameColumn]);
 					if (contigIndex == null) {
